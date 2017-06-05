4017b3ee3525076a667647f6285e1fcc3c9bd253
Justin LeFurjah <lefurjah@gmail.com>
2017-06-05 12:18:04 -0500

    add docs and doc gen / deploy script

---
+++ doc_source/VivialConnect/Resources/Attachment.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace VivialConnect\Resources;
+
+/**
+* For manipulating Attachment resources
+*/
+
+class Attachment extends Resource { }
---
+++ doc_source/VivialConnect/Resources/User.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace VivialConnect\Resources;
+
+/**
+* For manipulating User resources
+*/
+
+class User extends Resource { }
